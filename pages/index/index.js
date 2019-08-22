af6241f9ad9b0c17d399ebf502883a2c3a537010
Yuxiao Wang <me@rayrw.com>
2019-08-22 23:04:31 +0800

    update img url

--- pages/index/index.js
+++ pages/index/index.js
@@ -1,5 +1,6 @@
 Page({
   data: {
-    greeting: 'afuxiu'
+    greeting: 'afuxiu',
+    imageUrl: "https://rayrw-cn.oss-cn-shenzhen.aliyuncs.com/miniprogram/mana.png?Expires=1566489797&OSSAccessKeyId=TMP.hXwTq2pAbhEghhozh4rbfEHASmfe5qeq8zF1pHyCD2fWArjnoVFEugPFV96oaDQWWpzaNGewzKuChPgnnyFvEdM1GGBUj8qNNm1xAV7LhVF4UDtLmzK2RjUNovBmzc.tmp&Signature=uusZH1RaM%2FRcYTVCiiWvp02YrNw%3D"
   }
 })
