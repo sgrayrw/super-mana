4e11322f4fdd79ec226dd1fa330da736ab2541ec
Yuxiao Wang <me@rayrw.com>
2019-08-23 10:30:33 +0800

    update img

--- pages/index/index.js
+++ pages/index/index.js
@@ -1,6 +1,6 @@
 Page({
   data: {
-    greeting: 'afuxiu',
-    imageUrl: "https://rayrw-cn.oss-cn-shenzhen.aliyuncs.com/miniprogram/mana.png?Expires=1566489797&OSSAccessKeyId=TMP.hXwTq2pAbhEghhozh4rbfEHASmfe5qeq8zF1pHyCD2fWArjnoVFEugPFV96oaDQWWpzaNGewzKuChPgnnyFvEdM1GGBUj8qNNm1xAV7LhVF4UDtLmzK2RjUNovBmzc.tmp&Signature=uusZH1RaM%2FRcYTVCiiWvp02YrNw%3D"
+    greeting: "起床ne",
+    frontImgUrl: "https://rayrw-cn.oss-cn-shenzhen.aliyuncs.com/miniprogram/IMG_6948.JPG?OSSAccessKeyId=TMP.hXS864Y6HQm7jJ9SBuLHHM3pzsikZb5f63jSpWzoWAMXJYDNDr6X71vZuVLnGb5coP3VPu2VX3HmMTiKMMr26ApQQAfggtwSuPwgkPouyBXL3xgJf6Aeg2fG7osgLa.tmp&Expires=1566563115&Signature=1CKA0QaZMuViSlZW4wmkBHSO8Oo%3D"
   }
 })
